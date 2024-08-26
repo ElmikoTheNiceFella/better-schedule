1a5b3aa20c51bf975e057c623aed77ad246ca756
Abd El Hakim Akhadkhou <akprogamer23@gmail.com>
2024-08-27 01:01:51 +0300

    fix: better contrast on course colors

--- constants.js
+++ constants.js
@@ -1,15 +1,15 @@
 const COLORS = {
-  "I10": "#28a745",
-  "H07": "#fd7e14",
-  "H12": "#28a745",
-  "C11": "#28a745",
-  "B05": "#6610f2",
+  "I10": "#1a8e18",
+  "H07": "#8e5f18",
+  "H12": "#1a8e18",
+  "C11": "#1a8e18",
+  "B05": "#55188e",
   "I09": "#343a40"
 }
 
-const DEMO = `Arabic Language I | Arabic Language 100 Section L09 | Class Begin: 08/25/2024 | Class End: 12/05/2024
+const DEMO = `Arabic Language I | Arabic Language 100 Section L25 | Class Begin: 08/25/2024 | Class End: 12/05/2024
 Registered
-08/25/2024 -- 12/05/2024   Sunday,Tuesday
+08/25/2024 -- 12/05/2024   Monday,Wednesday
 S
 M
 T
@@ -17,13 +17,13 @@ W
 T
 F
 S
-   03:30 PM - 04:45 PM Type: Class Location: All Building: I09- College of Law Room: A110
-Instructor: ElKhrashy, Youssry (Primary)
-CRN: 11045
+   11:00 AM - 12:15 PM Type: Class Location: Male Designated Area Building: BCR- Corridor Room: D222
+Instructor: Al-Rhawi, Mohammad (Primary)
+CRN: 14641
 Message: **Web Registered** | Hours: 3 | Level: Undergraduate | Campus: Male Designated Area | Schedule Type: Lecture | Instructional Method: Arabic | Grade Mode: Standard Letter | Waitlist Position: 0 | Notification Expires: None
-Calculus I | Mathematics 101 Section L01 | Class Begin: 08/25/2024 | Class End: 12/05/2024
+Calculus I | Mathematics 101 Section L02 | Class Begin: 08/25/2024 | Class End: 12/05/2024
 Registered
-08/25/2024 -- 12/05/2024   Sunday,Tuesday,Thursday
+08/25/2024 -- 12/05/2024   Monday,Wednesday
 S
 M
 T
@@ -31,10 +31,36 @@ W
 T
 F
 S
-   12:00 PM - 12:50 PM Type: Class Location: Male Designated Area Building: BCR- Corridor Room: I111
+   08:00 AM - 09:15 AM Type: Class Location: Male Designated Area Building: BCR- Corridor Room: I110
+Instructor: Ayari, Sahbi (Primary)
+CRN: 10112
+Calculus I | Mathematics 101 Section B04 | Class Begin: 08/25/2024 | Class End: 12/05/2024
+Registered
+08/25/2024 -- 12/05/2024   Tuesday
+S
+M
+T
+W
+T
+F
+S
+   08:00 AM - 08:50 AM Type: Lab Location: Male Designated Area Building: BCR- Corridor Room: I110
 Instructor: El-Sanfaz, Mustafa (Primary)
-CRN: 11333
-Calculus I | Mathematics 101 Section B02 | Class Begin: 08/25/2024 | Class End: 12/05/2024
+CRN: 11670
+English Language I Post Foundation | English Language & Literature 202 Section 011 | Class Begin: 08/25/2024 | Class End: 12/05/2024
+Registered
+08/25/2024 -- 12/05/2024   Monday,Wednesday
+S
+M
+T
+W
+T
+F
+S
+   03:30 PM - 04:45 PM Type: Class Location: Male Designated Area Building: B05- Main Men's Building Room: 0206
+Instructor: Bassyony, Manal (Primary)
+CRN: 11594
+Experimental General Chemistry I | Chemistry 103 Section L12 | Class Begin: 08/25/2024 | Class End: 12/05/2024
 Registered
 08/25/2024 -- 12/05/2024   Thursday
 S
@@ -44,12 +70,12 @@ W
 T
 F
 S
-   01:00 PM - 01:50 PM Type: Lab Location: Male Designated Area Building: BCR- Corridor Room: I110
-Instructor: Dabboorasad, Yousef (Primary)
-CRN: 11668
-History of Qatar | History 121 Section L06 | Class Begin: 08/25/2024 | Class End: 12/05/2024
+   09:00 AM - 11:50 AM Type: Lab Location: Male Designated Area Building: BCR- Corridor Room: E210
+Instructor: Elhamshary, Marwa (Primary)
+CRN: 10611
+Programming Concepts | Computer Science 151 Section B02 | Class Begin: 08/25/2024 | Class End: 12/05/2024
 Registered
-08/25/2024 -- 12/05/2024   Sunday,Tuesday,Thursday
+08/25/2024 -- 12/05/2024   Tuesday
 S
 M
 T
@@ -57,23 +83,10 @@ W
 T
 F
 S
-   05:00 PM - 05:50 PM Type: Class Location: All Building: H08- Business & Econ. Bldg. Room: D107
-Instructor: Hayajneh, Raed (Primary)
-CRN: 10590
-Introduction to Psychology | Education Psychology 201 Section L01 | Class Begin: 08/25/2024 | Class End: 12/05/2024
-Registered
-08/25/2024 -- 12/05/2024   Monday,Wednesday
-S
-M
-T
-W
-T
-F
-S
-   03:30 PM - 04:45 PM Type: Class Location: All Building: I10- College of Education Room: C202
-Instructor: ElSawaf, Mona
-CRN: 12787
-Islamic Culture | Dawa 111 Section L05 | Class Begin: 08/25/2024 | Class End: 12/05/2024
+   12:00 PM - 01:50 PM Type: Lab Location: All Building: H07- College of Engineering Room: C208
+Instructor: AlQawasma, Farah (Primary)
+CRN: 11470
+Programming Concepts | Computer Science 151 Section L03 | Class Begin: 08/25/2024 | Class End: 12/05/2024
 Registered
 08/25/2024 -- 12/05/2024   Monday,Wednesday
 S
@@ -83,9 +96,9 @@ W
 T
 F
 S
-   05:00 PM - 06:15 PM Type: Class Location: All Building: H08- Business & Econ. Bldg. Room: D214
-Instructor: Laabdi, Mourad (Primary)
-CRN: 12009`
+   09:30 AM - 10:45 AM Type: Class Location: All Building: H07- College of Engineering Room: C203
+Instructor: Ali, Zeyad (Primary)
+CRN: 13523`
 
 const TUTORIAL_LINK = "https://youtu.be/aMFaa-Q6E6c"
 
